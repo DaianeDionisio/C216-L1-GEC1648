b03515f1fd8e145f84a6a0f17b65f82bf52979be
Daiane Dionisio de Lima <daianedionisio2000@gmail.com>
2024-03-16 08:19:50 -0300

    pratica-0

--- pratica-0/src/index.js
+++ pratica-0/src/index.js
@@ -1,5 +1,5 @@
 let modulo = require('./module.js');
-let nome = "Daiane Dionisio de Lima - 1648";
+const nome = "Daiane Dionisio de Lima - 1648";
 
 saudacaoPersonalizada(nome);
 modulo.saudacao();
